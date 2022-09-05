fc30b05d4ed769aefc38e2112ab9fb67a64c3d6a
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-09-05 23:30:16 +0700

    initialize

---
+++ mockez-back/src/main/java/com/mockez/MockezBackApplication.java
@@ -0,0 +1,13 @@
+package com.mockez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockezBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockezBackApplication.class, args);
+    }
+
+}
