acd680ffa060435dcaa6110f596e41771285cc63
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-09-21 12:30:11 +0700

    MOC-16 Create form module

--- mockez-front/src/app/general/component/general/general.component.ts
+++ mockez-front/src/app/general/component/general/general.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class GeneralComponent implements OnInit {
 
   isIncludeCreateTable: boolean = false;
+  isShowPopup: boolean = false;
 
   constructor() { }
 
--- mockez-front/src/app/general/general.module.ts
+++ mockez-front/src/app/general/general.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { GeneralComponent } from './component/general/general.component';
 import { ROUTING } from './general.routing';
 import { NgIf } from '@angular/common';
+import { PopupComponent } from './component/popup/popup.component';
 
 @NgModule({
   imports: [ROUTING, NgIf],
   declarations: [
-    GeneralComponent
+    GeneralComponent,
+    PopupComponent
   ],
   exports: [
     GeneralComponent
