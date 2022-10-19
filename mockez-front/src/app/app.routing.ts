094e248d49228252d79655fcc091473adfa124e9
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2022-10-19 10:18:17 +0700

    MOC-36: Configure ROUTING

--- mockez-front/src/app/app.routing.ts
+++ mockez-front/src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { GENERAL } from './app.constant';
+import { GENERAL, PROJECT, SCHEMA } from './app.constant';
 
 const routes: Routes = [
   { path: '', redirectTo: GENERAL, pathMatch: 'full' },
-  { path: GENERAL, loadChildren: () => import('./general/general.module').then(m => m.GeneralModule) }
+  { path: GENERAL, loadChildren: () => import('./general/general.module').then(m => m.GeneralModule) },
+  { path: SCHEMA, loadChildren: () => import('./schema/schema.module').then(m => m.SchemaModule) },
+  { path: PROJECT, loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }
 ];
 
 export const ROUTING = RouterModule.forRoot(routes, { useHash: true });
