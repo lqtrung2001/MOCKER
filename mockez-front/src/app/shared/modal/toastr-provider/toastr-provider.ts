87d792de38c781fd5130e6629873691e09b3f15a
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-02-05 09:14:49 +0700

    FRONT-END toastr provider

---
+++ mockez-front/src/app/shared/modal/toastr-provider/toastr-provider.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { ModalService } from '@shared/modal/modal-service/modal-service.service';
+import {
+  ToastrModal,
+  ToastrModalCustomOptions,
+  ToastrModalOptions,
+  ToastrPosition,
+  ToastrType
+} from '@shared/modal/toastr-provider/toastr/toastr.modal';
+
+/**
+ * @author Do Quoc Viet
+ * @date 01/02/2023
+ */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastrProvider {
+  toastrPositions: ToastrPosition[] = [];
+  options: ToastrModalOptions;
+
+  constructor(
+    private modalService: ModalService
+  ) {
+    // Currently toastr index
+  }
+
+  showError(toastrModalOptions: ToastrModalOptions): void {
+    const index: number = this.init();
+    const options: ToastrModalCustomOptions = {
+      title: 'Error',
+      ...toastrModalOptions,
+      type: ToastrType.ERROR,
+      id: index
+    };
+    this.show(options);
+  }
+
+  showWarning(toastrModalOptions: ToastrModalOptions): void {
+    const index: number = this.init();
+    const options: ToastrModalCustomOptions = {
+      title: 'Warning',
+      ...toastrModalOptions,
+      type: ToastrType.WARNING,
+      id: index
+    };
+    this.show(options);
+  }
+
+  showInformation(toastrModalOptions: ToastrModalOptions): void {
+    const index: number = this.init();
+    const options: ToastrModalCustomOptions = {
+      title: 'Information',
+      ...toastrModalOptions,
+      type: ToastrType.INFORMATION,
+      id: index
+    };
+    this.show(options);
+  }
+
+  showSuccess(toastrModalOptions: ToastrModalOptions): void {
+    const index: number = this.init();
+    const options: ToastrModalCustomOptions = {
+      title: 'Success',
+      ...toastrModalOptions,
+      type: ToastrType.SUCCESS,
+      id: index
+    };
+    this.show(options);
+  }
+
+  init(): number {
+    const index: number = Math.random();
+    this.toastrPositions.forEach((toastrPosition: ToastrPosition, i) => {
+      i++;
+      toastrPosition.bottom = i * 170 + 40;
+      toastrPosition.zIndex = 100 - i;
+    });
+    this.toastrPositions.unshift({
+      id: index,
+      bottom: 40,
+      zIndex: 100
+    });
+    return index;
+  }
+
+  show(toastrModalCustomOptions: ToastrModalCustomOptions): void {
+    this.modalService.open(ToastrModal, toastrModalCustomOptions);
+  }
+
+}
