15ad92a22b4037d60b590acc06e4221283014e65
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-02-17 23:32:58 +0700

    [FE] change root folder name

---
+++ mocker-front/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}'
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'gray': '#F0F2F5'
+      }
+    }
+  },
+  plugins: [],
+  important: true
+};
