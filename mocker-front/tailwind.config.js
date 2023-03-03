057933afa0f28b5c71e90227525aaeab67d8e455
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-03-03 09:52:51 +0700

    MOC-20: [FE] Create customize CSS for using in TailwinCss.

--- mocker-front/tailwind.config.js
+++ mocker-front/tailwind.config.js
@@ -3,16 +3,36 @@ module.exports = {
   content: [
     './src/**/*.{html,ts}'
   ],
+  important: true,
+  plugins: [],
   theme: {
     extend: {
       backgroundColor: {
-        'gray': '#F0F2F5'
+        'blue': '#4285F4',
+        'blue-solid': '#172B4D',
+        'gray': '#F0F2F5',
+        'green': '#00CA4E',
+        'red': '#FF605C',
+        'yellow': '#FFBD44'
       },
       height: {
-        '300': '300px'
+        '300': '1200px'
+      },
+      minWidth: {
+        '300': '1200px'
+      },
+      textColor: {
+        'blue': '#4285F4',
+        'gray': '#6B7280',
+        'green': '#00CA4E',
+        'primary-solid': '#333',
+        'red': '#FF605C',
+        'yellow': '#FFBD44'
+      },
+      width: {
+        '49/100': '49%',
+        '9/10': '90%'
       }
     }
-  },
-  plugins: [],
-  important: true
+  }
 };
