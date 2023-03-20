58d5cc504f4dfbf69e3aed2577dbafbda42f462d
Luong Quoc Trung <lqtrung20122001@gmail.com>
2023-03-20 21:00:53 +0700

    MOC-35: [BE] Change Relation Type to Enum

--- mocker-back/src/main/java/com/mocker/domain/model/entity/TableRelation.java
+++ mocker-back/src/main/java/com/mocker/domain/model/entity/TableRelation.java
@@ -1,12 +1,23 @@
 package com.mocker.domain.model.entity;
 
-import com.mocker.domain.model.entity.enumeration.Gender;
 import com.mocker.domain.model.entity.enumeration.RelationType;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.util.UUID;
 
