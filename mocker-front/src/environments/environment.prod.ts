1bcd5fd968f608a614139749f5b9f5a1cf04f959
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-06-13 22:35:47 +0700

    Fix deploy

--- mocker-front/src/environments/environment.prod.ts
+++ mocker-front/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
   production: true,
-  BASE_URL: 'https://mockerbe.onrender.com',
-  API_URL: 'https://mockerbe.onrender.com/api/v1'
+  BASE_URL: 'http://188.166.227.21:8080/',
+  API_URL: 'http://188.166.227.21:8080//api/v1'
 };
