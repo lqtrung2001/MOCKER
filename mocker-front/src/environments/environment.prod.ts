c77779ee20b57c47f09cf63bef8b160e5ff86692
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-06-16 00:20:22 +0700

    Change the environment path.

--- mocker-front/src/environments/environment.prod.ts
+++ mocker-front/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
   production: true,
-  BASE_URL: 'http://188.166.227.21:8080/',
+  BASE_URL: 'http://188.166.227.21:8080',
   API_URL: 'http://188.166.227.21:8080/api/v1'
 };
